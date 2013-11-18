3ee5e619df393b30b3af8b449c16f7a99887fd4f
jeroenstevens <jeroenstevens@live.nl>
2013-11-18 21:22:29 +0100

    db

--- db/migrate/20131118101429_remove_columns_from_users.rb
+++ db/migrate/20131118101429_remove_columns_from_users.rb
@@ -1,5 +1,10 @@
 class RemoveColumnsFromUsers < ActiveRecord::Migration
-  def change
+  def self.up
+    add_column :users, :first_name, :string
+    add_column :users, :email, :string
+  end
+
+  def self.down
     remove_column :users, :first_name
     remove_column :users, :email
   end
