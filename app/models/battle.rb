542ab52a253931fe74ff72dd2314d153aac8a1b9
jeroenstevens <jeroenstevens@live.nl>
2013-11-20 10:59:41 +0100

    sidebar

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -6,7 +6,7 @@ class Battle < ActiveRecord::Base
   validates :host_id, presence: true
   validates :status, presence: true
   validates :title, presence: {message: "Moet ingevuld zijn"}, length: {maximum: 25}
-  validates :player_limit, presence: {message: "Moet ingevuld zijn"}, length: {minimum: 2}, :numericality => { :only_integer => true }
+  validates :player_limit, presence: {message: "Moet ingevuld zijn"}, :numericality => { :only_integer => true, greater_than: 1, less_than_or_equal_to: 16 }
   validates :start_date, presence: {message: "Moet ingevuld zijn"}
 
   after_create :create_host_team_relation
