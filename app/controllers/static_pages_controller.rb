0500d8c23783f178d10c5f41c45553611c3f2908
M. Kingma <mathijskingma@gmail.com>
2013-11-27 12:55:47 +0100

    Profile info voor dashboard gemaakt

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,9 @@ class StaticPagesController < ApplicationController
   skip_before_action :check_signin, only: [:homepage]
   skip_before_action :check_notifications, only: [:homepage]
 
+	def homepage
+	end
+	
   def gametype
 
   end
