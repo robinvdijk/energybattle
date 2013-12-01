b7042e85cd325e8e5927c7ad3fd4d5af4e193f19
jeroenstevens <jeroenstevens@live.nl>
2013-12-01 21:24:42 +0100

    disable input invite field when team limit reached

--- app/assets/javascripts/battles.js
+++ app/assets/javascripts/battles.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
+  // Zorgt dat als er geen join button is de box aansluit op de members
   if ($("form").hasClass("new_team_relation")) {
-    $('.teams-header').css("padding-bottom","6em")
-    $('.primary-button').css("margin-left","1em")
+    $('.teams-header').css("padding-bottom","6em");
+    $('.primary-button').css("margin-left","1em");
   }
+  // Zorgt ervoor dat de headrow op dezelfde hoogte blijft als die ernaast
+  // if($(".teams-headrow").prev().index() === 1 ){
+//     $(".teams-headrow").css("margin-top","2em");
+//   }
 });
