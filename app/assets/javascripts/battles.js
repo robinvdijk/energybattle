40c6bc9204a741eec92bff6c94d5f52f5852e935
jeroenstevens <jeroenstevens@live.nl>
2013-12-01 21:53:28 +0100

    verwijder overbodige battles.js

--- app/assets/javascripts/battles.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  // Zorgt dat als er geen join button is de box aansluit op de members
-  if ($("form").hasClass("new_team_relation")) {
-    $('.teams-header').css("padding-bottom","6em");
-    $('.primary-button').css("margin-left","1em");
-  }
-});
