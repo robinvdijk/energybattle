33b48d696e1a66a231bcadc9aa2bfddac6d957ae
M. Kingma <mathijskingma@gmail.com>
2013-12-12 15:03:00 +0100

    juiste date uit fb gehaald

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,8 @@ class User < ActiveRecord::Base
 
 
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+		user_birthday = auth.extra.raw_info.birthday
+		
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
     if user
       return user
@@ -32,7 +34,7 @@ class User < ActiveRecord::Base
           email:auth.info.email,
           password:Devise.friendly_token[0,20],
           avatar:auth.info.image,
-          birthday:auth.user_birthday
+          birthday:auth.extra.raw_info.birthday
         )
       end
     end
